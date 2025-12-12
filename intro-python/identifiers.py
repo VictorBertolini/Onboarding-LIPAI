30a42ba43d1c2db956c6a82e3506d98e43627589
Bertolini <victorbertolinidesousa@gmail.com>
2025-12-12 15:22:35 -0300

    Aula 2 - Identificadores

---
+++ intro-python/identifiers.py
@@ -0,0 +1,44 @@
+"""
+Aula 2 - LIPAI - Identificadores em Python
+"""
+
+# Palavras Reservadas
+# True, False, None, and, as, assert, break, class, continue, def, del, elif, else, except,
+# finally, for, from, global, if
+
+# Identificadores 
+# nome de variáveis, funções, classes
+# Devem iniciar com letra (a-z, A-Z) ou underscore (_)
+# Não podem ter espaços em branco
+
+nome = 'LIPAI'
+idade = 30
+
+Nome = 'João'
+nome_completo = 'Maria da Silva'
+
+
+# Boas práticas (clean code)
+c = 10
+contador = 10
+
+s = 10 + 20
+soma = 10 + 20
+
+# Constantes
+
+PI = 3.14159
+
+idade = 18
+
+if idade >= 18:
+    print("Maior de idade")
+else:
+    print("Menor de idade")
+
+
+MAIORIDADE = 18
+if idade >= MAIORIDADE:
+    print("Maior de idade")
+else:
+    print("Menor de idade")
